814548ad857efc73fe445d3b9bca1e3b46d13bf5
ijmckoy <Ikeman200@gmail.com>
2024-04-22 23:45:21 -0400

    add code to ser.js file to show notes on html

--- Server.js
+++ Server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const notes = require('./db/db.json');
 const path = require('path');
-
+const fs = require('fs');
+const noteid = require('./public/assets/js/noteID.js');
+const { readFromFile, readAndAppend } = require('./public/assets/js/fsUtils.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -26,24 +28,50 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/api/notes', (req,res) => res.json(notes));
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));  
+});
 
 app.post('/api/notes', (req, res) => {
-    // Let the client know that their POST request was received
-    res.json(`${req.method} request received`);
-  
-// Destructuring assignment for the items in req.body
+  // Let the client know that their POST request was received
+  res.json(`${req.method} request received`);
+
+  // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
-  if (product && review && username) {
+  if (title && text) {
     // Variable for the object we will save
-    const newNote = {
+    const newNote = [{
       title,
       text,
-    };
-
+      id: noteid(),
+    }];
 
     
+    // Obtain existing reviews
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews!')
+        );
+      }
+    });
+
 
     const response = {
       status: 'success',
@@ -57,16 +85,13 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
-    // Show the user agent information in the terminal
-    console.info(req.rawHeaders);
-  
-    // Log our request to the terminal
-    console.info(`${req.method} request received`);
-  });
-  
+
+// Show the user agent information in the terminal
+//console.info(req.rawHeaders);
+
+// Log our request to the terminal
+//console.info(`${req.method} request received`);
+
 
 
 
